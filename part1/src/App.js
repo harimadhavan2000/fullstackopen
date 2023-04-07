21a618f6fc46aca9d6532e38f81587302144f929
Hari <harimadhavan2000@gmail.com>
2023-04-07 09:20:22 +0530

    changed gitignore

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,22 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = ['Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
 
 export default App
